8851db24b839629e5878c632be7918fc1c1af065
Abhinav Gunwant <abhigunwant@gmail.com>
2022-11-14 23:10:18 +0530

    Adding Permissions for User.

--- dam-fe/src/store/workspace/UserState.tsx
+++ dam-fe/src/store/workspace/UserState.tsx
@@ -1,3 +1,5 @@
+import UserPermissions from "../../models/UserPermissions";
+
 export default interface UserState {
     // Session token
     sessionToken: string,
@@ -5,5 +7,8 @@ export default interface UserState {
     // Session token expiry timestamp
     sessionTokenExpiry: number,
 
+    permissions: UserPermissions,
+
     setSession: (token: string, exp: number) => void;
+    setPermissions: (permissions: UserPermissions) => void;
 }
--- dam-fe/src/store/workspace/UserStore.tsx
+++ dam-fe/src/store/workspace/UserStore.tsx
@@ -1,4 +1,5 @@
 import create from 'zustand';
+import UserPermissions from '../../models/UserPermissions';
 
 import UserState from './UserState';
 
@@ -9,11 +10,37 @@ const useUserStore = create<UserState>()((set, get) => ({
     // Expiry time
     sessionTokenExpiry: 0,
 
+    permissions: {
+        createImage: false,
+        readImage: false,
+        modifyImage: false,
+        deleteImage: false,
+        readRenditions: false,
+        createRenditions: false,
+        modifyRenditions: false,
+        deleteRenditions: false,
+        readProject: false,
+        createProject: false,
+        modifyProject: false,
+        deleteProject: false,
+        readUser: false,
+        createUser: false,
+        modifyUser: false,
+        deleteUser: false,
+        publish: false,
+        publishAll: false,
+        accessAllProjects: false,
+    },
+
     setSession: (token: string, exp: number) => set((state) => ({
         ...state,
         sessionToken: token,
         sessionTokenExpiry: exp,
-    }))
+    })),
+
+    setPermissions: (permissions: UserPermissions) => set((state) => ({
+        ...state, permissions
+    })),
 }));
 
 export default useUserStore;
