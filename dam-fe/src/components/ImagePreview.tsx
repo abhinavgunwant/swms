19d7d4ef4c175eac0c260ca538ea492caad87644
Abhinav Gunwant <abhigunwant@gmail.com>
2023-02-02 00:20:14 +0530

    Fixing zoom

--- dam-fe/src/components/ImagePreview.tsx
+++ dam-fe/src/components/ImagePreview.tsx
@@ -52,11 +52,27 @@ interface ImagePreviewProps {
     onClose: () => void,
 }
 
+/**
+ * Tells how the image will be sized.
+ * default - Loads image in it's original size and allows zoom-in and zoom-out.
+ * original - Resizes image to it's original size.
+ * screen - Fits the image to the screen.
+ *
+ * When the image fit is in either `original` or `fit` and it is resized, the image
+ * fit is moved to `default`.
+ */
+type ImageFit = 'default' | 'original' | 'screen';
+
+const ZOOM_INCREMENT = 0.25;
+
 export const ImagePreview = (props: ImagePreviewProps) => {
     const [ loading, setLoading ] = useState<boolean>(true);
+    const [ originalWidth, setOriginalWidth ] = useState<number>();
+    const [ originalHeight, setOriginalHeight ] = useState<number>();
+    const [ imageFit, setImageFit ] = useState<ImageFit>('default');
     const [ imagePath, setImagePath ] = useState<string>('');
     const [ imageId, setImageId ] = useState<number>(props?.imageId || -1);
-    const [ zoom, setZoom ] = useState<number>(100);
+    const [ zoom, setZoom ] = useState<number>(1);
 
     const [ _, startTransition ] = useTransition();
 
@@ -64,12 +80,27 @@ export const ImagePreview = (props: ImagePreviewProps) => {
     const imgSectionRef = useRef<HTMLDivElement>(null);
 
     const onImageLoaded = () => {
-        onResize();
-
         setLoading(false);
+
+        if (
+            typeof imageRef !== 'undefined'
+            && typeof imgSectionRef !== 'undefined'
+        ) {
+            const imgWidth = imageRef.current?.width;
+            const imgHeight = imageRef.current?.height;
+
+            if (
+                typeof imgWidth !== 'undefined'
+                && typeof imgHeight !== 'undefined'
+                && imageRef.current
+            ) {
+                setOriginalWidth(imageRef.current.width);
+                setOriginalHeight(imageRef.current.height);
+            }
+        }
     }
 
-    const onResize = () => {
+    const imageFitToWidth = () => {
         if (
             typeof imageRef === 'undefined'
             //&& typeof imageRef.current === 'undefined'
@@ -116,21 +147,43 @@ export const ImagePreview = (props: ImagePreviewProps) => {
                 }
             }
         }
+    };
+
+    const onZoom = (inOrOut: 'in' | 'out') => {
+        if (inOrOut === 'in') {
+            setZoom(zoom + ZOOM_INCREMENT);
+
+            return;
+        }
+
+        setZoom(zoom - ZOOM_INCREMENT);
     }
 
     useEffect(() => {
         if (!props.show) {
             setLoading(true);
+        } else {
+            setZoom(1);
+
         }
     }, [ props.show ]);
 
     useEffect(() => {
-        document.addEventListener('resize', onResize);
-
-        return () => {
-            document.removeEventListener('resize', onResize);
+        console.log('resizing', zoom);
+        if (
+            typeof imageRef === 'undefined'
+            && typeof imgSectionRef === 'undefined'
+        ) {
+            return;
         }
-    }, []);
+
+        if (imageRef.current && originalWidth && originalHeight) {
+            console.log(originalWidth * zoom);
+            console.log(originalHeight * zoom);
+            imageRef.current.width = originalWidth * zoom;
+            imageRef.current.height = originalHeight * zoom;
+        }
+    }, [ zoom ]);
 
     if (!props.show) {
         return null;
@@ -156,13 +209,13 @@ export const ImagePreview = (props: ImagePreviewProps) => {
             <ControlSection>
                 <ControlButton
                     aria-label="zoom in"
-                    onClick={ (e) => { console.log('Zoom In!'); } }>
+                    onClick={ (e) => { console.log('Zoom In!'); onZoom('in'); } }>
                     <ZoomIn style={{ fontSize: '2rem', color: '#ffffff' }} />
                 </ControlButton>
 
                 <ControlButton
                     aria-label="zoom out"
-                    onClick={ (e) => { console.log('Zoom Out'); } }>
+                    onClick={ (e) => { console.log('Zoom Out'); onZoom('out'); } }>
                     <ZoomOut style={{ fontSize: '2rem', color: '#ffffff' }} />
                 </ControlButton>
 
