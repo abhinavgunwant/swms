d8981bc163ae6bb9bdc46e9bf34278b1e863e52a
Abhinav Gunwant <abhigunwant@gmail.com>
2023-02-27 01:40:41 +0530

    Moving from CRA to Vite

---
+++ dam-fe/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+
+export default defineConfig({
+    server: {
+        port: 3000,
+        open: true,
+    },
+    plugins: [
+        react(),
+        viteTsconfigPaths(),
+        svgrPlugin(),
+    ],
+});
+
