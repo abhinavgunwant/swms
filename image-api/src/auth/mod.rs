7d091febc41b0c40bb8fd16973e6438469e1b0e9
Abhinav Gunwant <abhigunwant@gmail.com>
2023-09-21 21:11:18 +0530

    refactor: Cleaner AuthMiddleware

--- image-api/src/auth/mod.rs
+++ image-api/src/auth/mod.rs
@@ -1,6 +1,7 @@
 use std::future::{ ready, Ready };
 
 use actix_web::{
+    error::ErrorUnauthorized,
     FromRequest, Error as ActixError, HttpRequest, dev::Payload, http::header,
 };
 
@@ -16,38 +17,43 @@ pub struct AuthMiddleware {
     pub login_id: String, // a.k.a the "username"
 }
 
+/// Handles Request authorization
 impl FromRequest for AuthMiddleware {
     type Error = ActixError;
     type Future = Ready<Result<Self, Self::Error>>;
 
     fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
-        match req.headers().get(header::AUTHORIZATION) {
-            Some(auth_header) => {
-                match auth_header.to_str() {
-                    Ok(auth_header) => {
-                        // The value of authorization header is in the format:
-                        // `Bearer <jwt-token>`
-                        // So, get the token after 7th pos (i.e. after space)).
-                        let token = auth_header.split_at(7).1.to_string();
+        if let Some(token) = Self::get_session_token(req) {
+            if let Ok(token_data) = decode_jwt(&token) {
+                return ready(Ok(Self {
+                    authorized: true,
+                    login_id: token_data.sub
+                }));
+            }
+        }
 
-                        match decode_jwt(&token) {
-                            Ok(token_data) => {
-                                return ready(Ok(Self {
-                                    authorized: true,
-                                    login_id: token_data.sub
-                                }));
-                            }
+        if req.path() == "/api/admin/auth/refresh"
+            || req.path() == "/api/admin/auth/login" {
+            return ready(Ok(Self::default()));
+        }
 
-                            Err(_) => ready(Ok(Self::default())),
-                        }
-                    }
+        ready(Err(ErrorUnauthorized("UNAUTHORIZED")))
+    }
+}
 
-                    Err(_) => ready(Ok(Self::default())),
+impl AuthMiddleware {
+    /// Returns the session token if it exists on `Authorization` header.
+    /// `None` otherwise.
+    fn get_session_token(req: &HttpRequest) -> Option<String> {
+        if let Some(auth_hdr_opt) = req.headers().get(header::AUTHORIZATION) {
+            if let Ok(auth_header) = auth_hdr_opt.to_str() {
+                if !auth_header.is_empty() && auth_header.len() > 7 {
+                    return Some(auth_header.split_at(7).1.to_string());
                 }
             }
-
-            None => ready(Ok(Self::default())),
         }
+
+        None
     }
 }
 
