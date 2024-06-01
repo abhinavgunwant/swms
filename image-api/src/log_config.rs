818d671871e31ba5868d57bbfb4ce150401a30e4
Abhinav Gunwant <abhigunwant@gmail.com>
2024-06-02 05:01:24 +0530

    feat: default log output to stdout
    When no log config file is found, all log will be printed on stdout.

--- image-api/src/log_config.rs
+++ image-api/src/log_config.rs
@@ -1,7 +1,10 @@
 use std::path::Path;
 
-use log::warn;
-use log4rs::init_file;
+use log::{ warn, LevelFilter };
+use log4rs::{
+    append::console::ConsoleAppender, config::{ Appender, Config, Root },
+    encode::pattern::PatternEncoder, init_config, init_file
+};
 
 const DEFAULT_LOG_FILE: &str = "log4rs.yml";
 
@@ -9,7 +12,26 @@ pub fn init_logger() {
     if Path::new(DEFAULT_LOG_FILE).exists() {
         init_file(DEFAULT_LOG_FILE, Default::default()).unwrap();
     } else {
-        warn!("No logger config found. Running logger with default config.");
+        let date_pattern_encoder = Box::new(
+            PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S)} | {h({({l}):5.5})} | {f:>20.128}:{L:4.7} - {m}{n}")
+        );
+
+        let stdout = ConsoleAppender::builder()
+            .encoder(date_pattern_encoder)
+            .build();
+
+        let stdout_appender = Appender::builder()
+            .build("stdout", Box::new(stdout));
+
+        let config = Config::builder()
+            .appender(stdout_appender)
+            .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+            .unwrap();
+
+        init_config(config).unwrap();
+
+        warn!("Logging to `stdout`");
+        warn!("Please refer to the \"logging\" section in the documentation");
     }
 }
 
